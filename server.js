58cdb1e1fff9896c907d4f2b93ae0bc5a6eba405
davidsodaa <sodakolor@gmail.com>
2017-04-14 14:06:04 +0200

    Navbar

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var index = require('./routes/index');
-
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
